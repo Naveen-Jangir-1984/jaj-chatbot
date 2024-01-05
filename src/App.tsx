5df67ebf261f401d729c180a4d986b6defe308d9
NJ <monu.bits@gmail.com>
2024-01-05 13:51:06 +0530

    basic chat layout

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="head">head</div>
+      <div className="display">display</div>
+      <div className="input">
+        <textarea></textarea>
+        <button>Send</button>
+      </div>
+      <div className="foot">foot</div>
+    </div>
+  );
+}
+
+export default App;
